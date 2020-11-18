50f68763a3269f8d0ccd858dbf4242b4513e157e
Samuele <samuele@marconi.ias.informatik.tu-darmstadt.de>
2020-11-18 17:14:38 +0100

    ICA, PCA, PPCA, MPPCA with same interfaces

--- core/dimensionality_reduction.py
+++ core/dimensionality_reduction.py
@@ -5,6 +5,31 @@ from sklearn.decomposition import PCA as ExternalPCA, FastICA
 from core.dr_interface import DimensionalityReduction
 
 
+class ICA(DimensionalityReduction, FastICA):
+    """
+    TODO: understand if sk-learn is actually implementing PPCA instead of PCA
+    """
+
+    def __init__(self, *args, **kwargs):
+        DimensionalityReduction.__init__(self)
+        FastICA.__init__(self, *args, **kwargs)
+
+    def get_latent_dim(self):
+        return self.n_components_
+
+    def get_observed_dim(self):
+        return self.n_features_
+
+    def fit(self, X: np.ndarray):
+        return FastICA.fit(self, X)
+
+    def reconstruct(self, latent, noise=False):
+        return self.inverse_transform(latent)
+
+    def compress(self, observed: np.ndarray):
+        return self.transform(observed)
+
+
 class PCA(DimensionalityReduction, ExternalPCA):
     """
     TODO: understand if sk-learn is actually implementing PPCA instead of PCA
@@ -28,6 +53,65 @@ class PCA(DimensionalityReduction, ExternalPCA):
 
     def compress(self, observed: np.ndarray):
         return self.transform(observed)
+
+
+class PPCA(DimensionalityReduction, ExternalMPPCA):
+
+    def __init__(self, *args, **kwargs):
+        DimensionalityReduction.__init__(self)
+        ExternalMPPCA.__init__(self, n_components=1, *args, **kwargs)
+        self.n_features_ = None
+
+    def get_latent_dim(self):
+        return self.latent_dimension
+
+    def get_observed_dim(self):
+        return self.n_features_
+
+    def fit(self, X: np.ndarray):
+        self.n_features_ = X.shape[1]
+        return ExternalMPPCA.fit(self, X)
+
+    def reconstruct(self, latent, noise=False):
+        return np.array([self.sample_from_latent(0, lat, noise=noise) for lat in latent])
+
+    def compress(self, observed: np.ndarray):
+        ret = []
+        for obs in observed:
+            _, cluster, lat = self.reconstruction(obs, idx=np.array(range(self.get_observed_dim())))
+            ret.append(lat)
+        return np.array(ret)
+
+
+class MPPCA(DimensionalityReduction, ExternalMPPCA):
+    """
+    TODO: understand if sk-learn is actually implementing PPCA instead of PCA
+    """
+
+    def __init__(self, *args, **kwargs):
+        DimensionalityReduction.__init__(self)
+        ExternalMPPCA.__init__(self, *args, **kwargs)
+        self.n_features_ = None
+
+    def get_latent_dim(self):
+        return self.latent_dimension
+
+    def get_observed_dim(self):
+        return self.n_features_
+
+    def fit(self, X: np.ndarray):
+        self.n_features_ = X.shape[1]
+        return ExternalMPPCA.fit(self, X)
+
+    def reconstruct(self, latent, noise=False):
+        return np.array([self.sample_from_latent(int(lat[0]), lat[1:], noise=noise) for lat in latent])
+
+    def compress(self, observed: np.ndarray):
+        ret = []
+        for obs in observed:
+            _, cluster, lat = self.reconstruction(obs, idx=np.array(range(self.get_observed_dim())))
+            ret.append(np.concatenate([np.array([cluster]), lat]))
+        return np.array(ret)
 
 
 
--- tests/test_pca.py
+++ tests/test_pca.py
@@ -1,7 +1,7 @@
 import numpy as np
 import unittest
 
-from core.dimensionality_reduction import PCA
+from core.dimensionality_reduction import PCA, PPCA, MPPCA
 
 
 class TestPCA(unittest.TestCase):
@@ -15,12 +15,30 @@ class TestPCA(unittest.TestCase):
         latent_dim = 3
         latent = np.random.multivariate_normal(np.zeros(latent_dim), np.eye(latent_dim), size=1000)
         lin = np.random.uniform(size=(obs_dim, latent_dim))
-        obs = lin @ latent.T
+        obs = latent @ lin.T
         pca = PCA(n_components=obs_dim)
         try:
             pca.fit(obs)
             L = pca.compress(obs)
             X = pca.reconstruct(L)
+            self.assertTrue(np.mean(np.abs(X - obs)) <= 0.01)
+        except NotImplemented:
+            self.fail("myFunc() raised ExceptionType unexpectedly!")
+
+        ppca = PPCA(latent_dimension=3)
+        try:
+            ppca.fit(obs)
+            L = ppca.compress(obs)
+            X = ppca.reconstruct(L)
+            self.assertTrue(np.mean(np.abs(X - obs)) <= 0.01)
+        except NotImplemented:
+            self.fail("myFunc() raised ExceptionType unexpectedly!")
+
+        mppca = MPPCA(n_components=2, latent_dimension=3)
+        try:
+            mppca.fit(obs)
+            L = mppca.compress(obs)
+            X = mppca.reconstruct(L)
             self.assertTrue(np.mean(np.abs(X - obs)) <= 0.01)
         except NotImplemented:
             self.fail("myFunc() raised ExceptionType unexpectedly!")
