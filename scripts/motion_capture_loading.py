ab697ad12e18877056f6ea24642d21e53142abb2
Jonas <jonas.stadtmueller@stud.tu-darmstadt.de>
2020-11-27 09:29:36 +0100

    Sorted files according to task; corrected file format

---
+++ scripts/motion_capture_loading.py
@@ -0,0 +1,8 @@
+import numpy as np
+from romi.trajectory import LoadTrajectory
+import pandas as pd
+
+#data = np.load('../datasets/motion_capture/amc_npy/143/143_01.npy', allow_pickle=True)
+#mocap = LoadTrajectory('../datasets/motion_capture/amc_npy/143/143_01.npy')
+pd.load_
+print()
